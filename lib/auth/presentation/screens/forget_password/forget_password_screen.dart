9f55fd65b76db44eeb4c37c9a023b455aae498b9
Ahmed Shiref <103084673+Ahmedshiref72@users.noreply.github.com>
2023-01-27 20:17:13 +0200

    final

--- lib/auth/presentation/screens/forget_password/forget_password_screen.dart
+++ lib/auth/presentation/screens/forget_password/forget_password_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
